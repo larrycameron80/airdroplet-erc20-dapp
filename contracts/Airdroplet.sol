2a7a2254fd364ede4fbd3c484292649f06e9b44b
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-04-29 22:18:27 +0100

    Create Airdroplet.sol

---
+++ contracts/Airdroplet.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.18;
+
+import "./SafeMath.sol";
+import "./ERC20.sol";
+
+contract Airdroplet
+{
+
+    ERC20 public token;
+    using SafeMath for uint256;
+
+    function airdropExecute(address source, address[] recipents, uint256 airdrop) public
+    {
+
+        uint x = 0;
+        token = ERC20(source);
+
+        while(x++ < recipents.length)
+        {
+
+            token.transferFrom(msg.sender, recipents[x], airdrop);
+
+        }
+
+
+    }
+
+
+}
