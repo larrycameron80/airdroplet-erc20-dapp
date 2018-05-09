b782ddb6666fcf2bdd9e80a32f99e48033580bf3
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-05-09 15:10:27 +0100

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var Airdroplet = artifacts.require("./Airdroplet.sol");
+var Token = artifacts.require("./Token.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Token);
   deployer.deploy(Airdroplet);
 
 };
